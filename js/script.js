e14108a87bceb1c404a95dcb496f90efbda01693
Anderson Araujo <andersonaraujoc1@gmail.com>
2022-05-09 21:20:49 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -1,17 +1,83 @@
 class Produto {
     constructor() {
-        this.id = 0;
-        this.nomeProduto = '';
-        this.valor = 0;
+        this.id = 1;
+        this.arrayProdutos = [];
     }
 
-    adicionar() {
-        console.log('ADicionado')
-        // ..
+    salvar() {
+        let produto = this.lerDados();
+
+        if (this.validaCampos(produto)) {
+            this.adicionar(produto);
+        }
+
+        this.listTabela();
+        this.cancelar();
     }
 
-    excluir() {
-        console.log('Deletado')
+    listTabela() {
+        let tbody = document.getElementById("tbody");
+
+        tbody.innerText = "";
+
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_produto = tr.insertCell();
+            let td_valor = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+            td_id.innerText = this.arrayProdutos[i].id;
+            td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+            td_valor.innerText = this.arrayProdutos[i].preco;
+
+            td_id.classList.add("center");
+
+            let imgEdit = document.createElement("img");
+            imgEdit.src = "./assets/icons8-edit.svg";
+
+            let imgDelete = document.createElement("img");
+            imgDelete.src = "./assets/icons8-delete.svg";
+
+            td_acoes.appendChild(imgEdit);
+            td_acoes.appendChild(imgDelete);
+        }
+    }
+
+    adicionar(produto) {
+        this.arrayProdutos.push(produto);
+        this.id++;
+    }
+
+    lerDados() {
+        let produto = {};
+
+        produto.id = this.id;
+        produto.nomeProduto = document.getElementById("produto").value;
+        produto.preco = document.getElementById("preco").value;
+
+        return produto;
+    }
+
+    validaCampos(produto) {
+        let msg = "";
+        if (produto.nomeProduto == "") {
+            msg += "- Informe o nome do produto \n";
+        }
+        if (produto.preco == "") {
+            msg += "- Informe o Preço do produto \n";
+        }
+        if (msg != "") {
+            alert(msg);
+            return false;
+        }
+        return true;
+    }
+
+    cancelar() {
+        document.getElementById("produto").value = "";
+        document.getElementById("preco").value = "";
     }
 }
 
